5c7fceb24c4a27a09b9bac430db53b9b25610f18
username-404-rgb <alexander.adams4@education.nsw.gov.au>
2025-03-27 15:08:56 +1100

    attempted adding more functions

--- main.py
+++ main.py
@@ -6,18 +6,22 @@ from spellbook import *
 def main():
     print ('Welcome to the spell helper')
     print ('1 - Search for a spell')
-    print ('2 - View Spellbook')
-    print ('3 - Remove spell from Spellbook')
-    print ('4 - Quit Program')
+    print ('2 - Add spell to spellbook')
+    print ('3 - View Spellbook')
+    print ('4 - Remove spell from Spellbook')
+    print ('5 - Quit Program')
     print ('')
     choice = input('What Do You Choose?')
     if choice == '1':
         use_search()
+        print('Back to main') #TODO
     elif choice == '2':
-        print ('Spellbook') #TODO
+        use_add
     elif choice == '3':
-        print ('3 done') #TODO
+        print ('Spellbook') #TODO
     elif choice == '4':
+        print ('remove form book') #TODO
+    elif choice == '5':
         print ('QUIT') #TODO
     else:
         print ('Err')
